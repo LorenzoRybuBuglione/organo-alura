8a9bebe0f9e20dcff4e60d780172974073e0ba9a
Lorenzo Rybu Buglione <lorenzorybuglione@gmail.com>
2023-06-07 11:09:06 -0300

    Don't show empty teams

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -3,19 +3,24 @@ import "./Team.css";
 
 const Team = (props) => {
   return (
-    <section className="team" style={{ backgroundColor: props.secundaryColor }}>
-      <h3 style={{ borderColor: props.primaryColor }}>{props.name}</h3>
-      <div className="workers">
-        {props.workers.map((worker) => (
-          <WorkerCard
-            key={worker.name}
-            name={worker.name}
-            role={worker.role}
-            image={worker.image}
-          />
-        ))}
-      </div>
-    </section>
+    props.workers.length > 0 && (
+      <section
+        className="team"
+        style={{ backgroundColor: props.secundaryColor }}
+      >
+        <h3 style={{ borderColor: props.primaryColor }}>{props.name}</h3>
+        <div className="workers">
+          {props.workers.map((worker) => (
+            <WorkerCard
+              key={worker.name}
+              name={worker.name}
+              role={worker.role}
+              image={worker.image}
+            />
+          ))}
+        </div>
+      </section>
+    )
   );
 };
 export default Team;
